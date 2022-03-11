d2948786bfb399044848877a9cdec7597e1c1a2d
NGUYEN HOAI TRUNG <nghtrung1001@gmail.com>
2022-03-11 14:00:37 +0900

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,15 +3,21 @@ import { Link } from 'react-router-dom'
 import Seachbar from './Seachbar'
 function Navbar() {
   return (
-    <nav  className='flex justify-between p-5 mx-auto w-full bg-red-300 items-center text-white' >
+    <nav  className='flex justify-between p-1 md:p-5 mx-auto w-full bg-red-300 items-center text-white' >
+      <div className='hidden md:flex md:text-4xl'>
         <Link to='/'>
-            <h1 className='text-4xl ml-10'> Cooking Recipe </h1>
+            <h1 className=''> Cooking Recipe </h1>
         </Link>
-        <div className='flex items-center justify-end mr-10'>
-          <Seachbar className=''/>
-          <Link to='/create'>
-              <div className='text-3xl border-2 rounded-xl p-3'>Create Recipe</div>
-          </Link>
+      </div>
+        <div className='flex items-center md:justify-end justify-between'>
+          <div>
+            <Seachbar />
+          </div>
+          <div className='text-sm	md:text-lg border-2 rounded-xl py-1 px-1 md:p-3 text-center'>
+            <Link to='/create'>
+                <div >Create Recipe</div>
+            </Link>
+          </div>
         </div>
     </nav>
   )
